cfd83b89c8d0e4267d461d1e9f33bbe7e245db13
kady-debug <123380428+kady-debug@users.noreply.github.com>
2024-04-28 15:59:18 +0200

    modification de la page education

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,66 +1,133 @@
-import React, { useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { FaSchool } from "react-icons/fa";
+import { gsap, Power3 } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Layout, Steps, Button } from "antd";
+import LangageIcons from "../layouts/LangageIcons";
+
+const { Content } = Layout;
+const { Step } = Steps;
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Education = () => {
-    const Accordion = ({ title, content }) => {
-        const [isOpen, setIsOpen] = useState(false);
+    const [currentStep, setCurrentStep] = useState(0); // État de l'étape actuelle
 
-        const toggleAccordion = () => {
-            setIsOpen(!isOpen);
-        };
+    const animateElements = (textElement, iconElement) => {
+        gsap.set([textElement, iconElement], { opacity: 0 });
 
-        return (
-            <div className="accordion mb-4">
-                <div
-                    className="accordion-title cursor-pointer py-2 px-4 bg-boutonBase text-white text-center hover:bg-brightColor"
-                    onClick={toggleAccordion}
-                >
-                    {title}
-                </div>
-                {isOpen && (
-                    <div className="accordion-content py-2 px-4 bg-experience">
-                        {content}
-                    </div>
-                )}
-            </div>
+        gsap.fromTo(
+            iconElement,
+            { y: -200, opacity: 0 },
+            {
+                y: 0,
+                opacity: 1,
+                duration: 2,
+                ease: Power3.easeOut,
+            }
+        );
+
+        gsap.fromTo(
+            textElement,
+            { y: -200, opacity: 0 },
+            {
+                y: 0,
+                opacity: 1,
+                duration: 3,
+                ease: Power3.easeOut,
+            }
         );
     };
 
+    useEffect(() => {
+        const textElement = document.querySelector(".text-animation");
+        const iconElement = document.querySelector(".icon-animation");
+
+        animateElements(textElement, iconElement);
+
+        const scrollTrigger = ScrollTrigger.create({
+            trigger: textElement,
+            start: "top center",
+            onEnter: () => {
+                animateElements(textElement, iconElement);
+            },
+        });
+
+        return () => {
+            // Limitez le nettoyage de ScrollTrigger lors du démontage du composant
+            scrollTrigger.kill();
+        };
+    }, []);
+
+    // Fonction pour avancer à l'étape suivante
+    const nextStep = () => {
+        setCurrentStep(currentStep + 1);
+    };
+
+    // Fonction pour revenir à l'étape précédente
+    const prevStep = () => {
+        setCurrentStep(currentStep - 1);
+    };
+
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen ">
-            <div className="p-6 text-center">
-                <h1 className="text-3xl font-bold mb-4 m-auto font-ligth mb-8 text-boutonBase hover:text-brightColor text-center md:w-1/2">
-                    Éducation
-                </h1>
-                <p className="w-11/12 md:w-5/6 m-auto text-black bold-light">
-                    Nous et nos partenaires stockons et/ou accédons à des informations sur votre appareil, telles que des cookies et traitons des données personnelles,
-                    telles que des identifiants uniques et des informations standard envoyées par un appareil pour des publicités et du contenu personnalisés,
-                    des mesures d'audience et des informations sur l'audience, ainsi que pour développer et améliorer nos produits.
-                    Avec votre autorisation, nous et nos partenaires pouvons utiliser des données de géolocalisation précises et identifier un scan de dispositif.
-                </p>
+        <Content
+            style={{
+                minHeight: "90vh", // Augmentation de la hauteur minimale
+                marginRight: "50px",
+                marginLeft: "350px", // Ajustement de la marge à gauche pour le menu vertical
+                display: "flex",
+                flexDirection: "column", // Affichage en colonne
+                justifyContent: "flex-start", // Aligner le contenu en haut
+                alignItems: "center", // Centrage vertical
+                marginBottom: "20px",
+                overflowX: "hidden",
+                position: "relative",
+                zIndex: 1,
+            }}
+        >
+            <div className="mt-20 mb-20">
+                <LangageIcons />
             </div>
-            <div className="flex flex-col md:flex-row mx-4 md:mx-auto mt-8 w-11/12 md:w-5/6 h-full bg-experience shadow-nuanceBlack shadow-md rounded-md">
-                <div className="w-full md:w-1/2 flex flex-col items-center justify-center m-4 bg-opacity-20 ">
-                    <h2 className="text-3xl font-bold mb-4 text-boutonBase hover:text-brightColor">Cursus au Mali</h2>
-                    <Accordion title="2011-2012" content="et/ou accéder à des informations sur un appareil, telles que des cookies et traiter des données personnelles, 
-                        telles que des identifiants uniques" />
-                    <Accordion title="2012-2014" content="et/ou accéder à des informations sur un appareil, telles que des cookies et traiter des données personnelles, 
-                        telles que des identifiants uniques" />
+
+            <div className="flex flex-col justify-center lg:flex-row items-center w-3/4 px-5 bg-brunClaire h-20 m-10">
+                <div className="flex flex-col items-center text-center lg:text-start lg:items-start lg:w-5/6 m-4 space-y text-animation m-auto">
+                    <h1 className="text-5xl font-semibold text-iconBrun leading-tight m-4">
+                        Etudes et Formations
+                        <span className="block text-hoverBouton m-4">
+                            {" "}
+                            Au Mali et en Belgique
+                        </span>
+                    </h1>
+                    <p className="text-texte w-full">
+                        J'opte pour la passionnante démarche de me former en développement
+                        frontend, nourrissant mon intérêt pour la création des expériences
+                        utilisateur exceptionnelles.
+                    </p>
                 </div>
-                <div className="w-full md:w-1/2 flex flex-col items-center justify-center m-4 ">
-                    <h2 className="text-3xl font-bold mb-4 text-boutonBase hover:text-brightColor">Cursus en Belgique</h2>
-                    <Accordion title="2017-2019" content="et/ou accéder à des informations sur un appareil, telles que des cookies et traiter des données personnelles, 
-                        telles que des identifiants uniques" />
-                    <Accordion title="2023-2024" content="et/ou accéder à des informations sur un appareil, telles que des cookies et traiter des données personnelles, 
-                        telles que des identifiants uniques" />
+
+                <div className="flex items-center justify-center icon-animation">
+                    <FaSchool className="text-9xl text-cc" />
                 </div>
             </div>
-            {/* <div className="mt-4">
-                <RouterLink to="/" className="text-brightColor hover:underline">
-                    Aller à la page d'accueil
-                </RouterLink>
-            </div> */}
-        </div>
+
+            <div className="w-5/6 px-5 mt-10 mb-10 mt-auto">
+                <Steps direction="horizontal" current={currentStep} className="mb-8">
+                    <Step title="2011-2012" description="Cursus au Mali" />
+                    <Step title="2012-2014" description="Cursus au Mali" />
+                    <Step title="2017-2019" description="Cursus en Belgique" />
+                    <Step title="2023-2024" description="Cursus en Belgique" />
+                </Steps>
+                <div className="flex justify-center">
+                    <Button disabled={currentStep === 0} onClick={prevStep}>
+                        Précédent
+                    </Button>
+                    <div style={{ width: "16px" }}></div> {/* Espace entre les boutons */}
+                    <Button disabled={currentStep === 3} onClick={nextStep}>
+                        Suivant
+                    </Button>
+                </div>
+            </div>
+        </Content>
     );
 };
 
--- src/layouts/LangageIcons.jsx
+++ src/layouts/LangageIcons.jsx
@@ -4,11 +4,11 @@ import { SiHtml5, SiCss3, SiJavascript, SiReact, SiPython } from "react-icons/si
 const LangageIcons = () => {
     return (
         <div className="flex items-center justify-center space-x-8 mt-50 left-50">
-            <SiHtml5 size={40} style={{ color: "#9f7a56", marginRight: "16px" }} />
-            <SiCss3 size={40} style={{ color: "#9f7a56", marginRight: "16px" }} />
-            <SiJavascript size={40} style={{ color: "#9f7a56", marginRight: "16px" }} />
-            <SiReact size={40} style={{ color: "#9f7a56", marginRight: "16px" }} />
-            <SiPython size={40} style={{ color: "#9f7a56", marginRight: "16px" }} />
+            <SiHtml5 size={40} style={{ color: "#45352a", marginRight: "16px" }} />
+            <SiCss3 size={40} style={{ color: "#45352a", marginRight: "16px" }} />
+            <SiJavascript size={40} style={{ color: "#45352a", marginRight: "16px" }} />
+            <SiReact size={40} style={{ color: "#45352a", marginRight: "16px" }} />
+            <SiPython size={40} style={{ color: "#45352a", marginRight: "16px" }} />
         </div>
     );
 };
